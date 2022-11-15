35cfe542af088e05903a5f5bd8aeac3c2f66dc7c
Francesc Requesens <francesc.requesens@gmail.com>
2022-11-15 22:31:39 +0100

    Challenge 02

---
+++ challenge2/catchCybercriminals.js
@@ -0,0 +1,25 @@
+const catchCybercriminals = (input) => {
+    if (typeof input !== 'string' || input === '') {
+        throw new Error('Invalid argument');
+    }
+    const asciiWords = input.split(' ');
+    const words = asciiWords.map(asciiWord => {
+        if (isNaN(asciiWord) || asciiWord < 0) {
+            throw new Error('Invalid argument');
+        }
+        const asciiChars = asciiWord.match(/(9.{1}|1.{2})/g);
+        if (asciiChars === null) {
+            return '';
+        }
+        const chars = asciiChars.map(asciiChar => {
+            if (asciiChar < 97 || asciiChar > 122) {
+                return '';
+            }
+            return String.fromCharCode(asciiChar);
+        });
+        return chars.join('');
+    });
+    return words.join(' ');    
+};
+
+export { catchCybercriminals };
---
+++ tests/challenge2/catchCybercriminals.test.js
@@ -0,0 +1,59 @@
+import { describe, it, expect } from 'vitest';
+import { catchCybercriminals } from '../../challenge2/catchCybercriminals';
+
+describe('Catch cybercriminals test', () => {
+    it('has a function called catchCybercriminals', () => {
+        expect(typeof catchCybercriminals).toBe('function');
+    });
+
+    it('throws an error if parameter is not a string', () => {
+        expect(() => catchCybercriminals()).toThrow('Invalid argument');
+    });
+
+    it('throws an error if characters are not a natural numbers', () => {
+        expect(() => catchCybercriminals("a")).toThrow('Invalid argument');
+    });
+
+    it('returns a if input is 97', () => {
+        expect(catchCybercriminals('97')).toBe('a');
+    });
+
+    it('returns z if input is 122', () => {
+        expect(catchCybercriminals('122')).toBe('z');
+    });
+
+    it('returns empty string if input is 96 or lower. Only basic lower characters', () => {
+        expect(catchCybercriminals('96')).toBe('');
+        expect(catchCybercriminals('12')).toBe('');
+    });
+
+    it('returns empty string if input is 123 or greather. Only basic lower characters', () => {
+        expect(catchCybercriminals('123')).toBe('');
+        expect(catchCybercriminals('882')).toBe('');
+    });
+    
+    it('returns empty words', () => {
+        expect(catchCybercriminals('485785476 8457 8457')).toBe('  ');
+    });
+
+    it('returns midu if input is 109105100117', () => {
+        expect(catchCybercriminals('109105100117')).toBe('midu');
+    });
+
+    it('returns codember if input is 9911110010110998101114', () => {
+        expect(catchCybercriminals('9911110010110998101114')).toBe('codember');
+    });
+
+    it('returns codember midu if input is 9911110010110998101114 109105100117', () => {
+        expect(catchCybercriminals('9911110010110998101114 109105100117')).toBe('codember midu');
+    });
+
+    it('returns play tetris if input is 11210897121 116101116114105115', () => {
+        expect(catchCybercriminals('11210897121 116101116114105115')).toBe('play tetris');
+    });
+
+    it('resolve 11610497110107115 102111114 11210897121105110103 9911110010110998101114 11210810197115101 11510497114101', () => {
+        console.log(catchCybercriminals('11610497110107115 102111114 11210897121105110103 9911110010110998101114 11210810197115101'));
+    });
+
+});
